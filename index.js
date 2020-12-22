29d7b540f1ecb9b3ebed967fc7e5143571172fec
DigitalHDR <digitalextremeultra@gmail.com>
2020-12-22 19:19:42 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var fields = document.querySelectorAll(".textb input")
+var btn = document.querySelector(".btn")
+
+function check() {
+    if (fields[0].value != "" && fields[1].value != "") {
+        btn.disabled = false
+    } else {
+        btn.disabled = true
+    }   
+}
+
+fields[0].addEventListener("keyup", check)
+fields[1].addEventListener("keyup", check)
+
+let mostrarSenha = document.querySelector(".show-password")
+mostrarSenha.addEventListener("click", inicia)
+
+function inicia() {
+    if (this.classList[2] == "fa-eye-slash") {
+        this.classList.remove("fa-eye-slash")
+        this.classList.add("fa-eye")
+        fields[1].type = "text"
+    } else {
+        this.classList.remove("fa-eye")
+        this.classList.add("fa-eye-slash")
+        fields[1].type = "password"
+    }
+}
